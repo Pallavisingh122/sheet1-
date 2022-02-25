855a7220ed11827fdd700f2cf3f7e55c7984fc14
Pallavisingh122 <100432786+Pallavisingh122@users.noreply.github.com>
2022-02-25 23:50:19 +0530

    Add files via upload

---
+++ sheet1.ty.py
@@ -0,0 +1,26 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5**9
+1953125
+3//2
+1
+7//3
+2
+7/3
+2.3333333333333335
+6==6
+True
+a=20;a+=30;a%=3;print(a)
+2
+True*False
+0
+True&False
+False
+True and False
+False
+((6>3) and (7<4) or (18==3)) and (9>3)
+False
+True is False
+False
+((True==False) or (False>True)) and (False<=True)
+False
